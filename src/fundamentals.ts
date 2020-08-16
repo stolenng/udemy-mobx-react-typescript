47c2db72c8148a3923cfa253d3f686fc0f0e9b16
stolenng <stolenng@gmail.com>
2020-08-17 00:57:35 +0300

    actions

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -1,29 +1,47 @@
-import {observable} from "mobx";
+import {action, autorun, observable, runInAction} from "mobx";
 
-let person = observable({
-    firstName: 'Mobx',
-    lastName: 'React'
-});
+interface IPerson {
+    firstName: string;
+    lastName: string;
+}
+
+const waitForPromise = async () => new Promise(resolve => setTimeout(resolve, 1000));
 
 class Person {
     @observable
     firstName: string = 'Mobx';
     @observable
-    lastName: string = 'React'
+    lastName: string = 'React';
 
-    constructor(props: any) {
+    constructor(props: IPerson) {
         Object.assign(this, props);
     }
+
+    // @action
+    // updateFullName(name: string, lastName: string) {
+    //     this.firstName = name;
+    //     this.lastName = lastName;
+    // }
 }
 
-console.log(person);
-
-const classPerson = new Person({
-    firstName: 'Class',
-    lastName: 'Observable'
+const ourPerson = new Person({
+    firstName: 'Mobx',
+    lastName: 'React'
 });
 
-console.log(classPerson);
+// for logging for now.
+autorun(() => {
+    console.log(`${ourPerson.firstName} ${ourPerson.lastName}`);
+});
+
+const updater = action(async () => {
+    ourPerson.lastName = 'new Name';
+    await waitForPromise();
+
+    ourPerson.firstName = 'new last name'
+});
+
+updater();
 
 
 
