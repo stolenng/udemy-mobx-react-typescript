c7b19308f15cc8f9cbf577a44b741c21f7f90d2c
stolenng <stolenng@gmail.com>
2020-08-27 22:35:39 +0300

    react finally

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// 
+import 'mobx-react-lite/batchingForReactDom';
+
 // current working file
 import './fundamentals';
 
